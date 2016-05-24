e23ac6358efd57ada0af3518774896edcbc26fb7
Jose Miguel López Bayona <jmlb0003@gmail.com>
2016-05-24 16:12:14 +0200

    Add menu support in MainActivityFragment and the method startActivity towards SettingsActivity

--- app/src/main/java/com/josecognizant/popmovies/ui/movie_grid/MainActivityFragment.java
+++ app/src/main/java/com/josecognizant/popmovies/ui/movie_grid/MainActivityFragment.java
@@ -1,17 +1,23 @@
 package com.josecognizant.popmovies.ui.movie_grid;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.josecognizant.popmovies.R;
+import com.josecognizant.popmovies.presentation.movie_grid.MovieGridView;
+import com.josecognizant.popmovies.ui.settings.SettingsActivity;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements MovieGridView {
 
     public MainActivityFragment() {
     }
@@ -19,6 +25,29 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_gridview_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startSettingsActivity();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void startSettingsActivity() {
+        Intent intent = new Intent(getActivity(), SettingsActivity.class);
+        startActivity(intent);
     }
 }
