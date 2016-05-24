bfbb89760f6d1d7843c1605844de0388630f97eb
Jose Miguel López Bayona <jmlb0003@gmail.com>
2016-05-24 16:00:34 +0200

    Add a GridLayout in MainAc tivityFragment

--- app/src/main/java/com/josecognizant/popmovies/ui/movie_grid/MainActivityFragment.java
+++ app/src/main/java/com/josecognizant/popmovies/ui/movie_grid/MainActivityFragment.java
@@ -1,10 +1,12 @@
-package com.josecognizant.popmovies;
+package com.josecognizant.popmovies.ui.movie_grid;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.josecognizant.popmovies.R;
 
 /**
  * A placeholder fragment containing a simple view.
