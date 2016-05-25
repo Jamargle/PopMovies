1e913fd5c887d51d1ddb450f8f80022f36ff494f
Jose Miguel López Bayona <jmlb0003@gmail.com>
2016-05-25 15:13:49 +0200

    Add a class to parse data from Moviedb API

--- app/src/main/java/com/josecognizant/popmovies/MovieJsonParserApiClient.java
+++ app/src/main/java/com/josecognizant/popmovies/MovieJsonParserApiClient.java
@@ -1,7 +1,52 @@
 package com.josecognizant.popmovies;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by 552702 on 25/05/2016.
+ * JSON parser for parsing movie data fetched from Moviedb API
+ * Created by Jose on 24/05/2016.
  */
 public class MovieJsonParserApiClient {
+    private static final String RESULTS_PARAMETER = "results";
+    private static final String POSTER_PATH_PARAMETER = "poster_path";
+    private static final String ORIGINAL_TITLE_PARAMETER = "original_title";
+    private static final String OVERVIEW_PARAMETER = "overview";
+    private static final String USER_RATING_PARAMETER = "vote_count";
+    private static final String RELEASE_DATE_PARAMETER = "release_date";
+
+    private static JSONArray sMovieData;
+
+    /**
+     * Take a movie string in JSON Format and extract the data in a List of Movies
+     *
+     * @return List of Movies with the data from the API
+     * @throws JSONException
+     */
+    public static List<Movie> getMoviesDataFromJson(String jsonData) throws JSONException {
+        JSONObject movies = new JSONObject(jsonData);
+        sMovieData = movies.getJSONArray(RESULTS_PARAMETER);
+
+        List<Movie> moviesItems = new ArrayList<>();
+        for (int i = 0; i < sMovieData.length(); i++) {
+            Movie movie = extractMovie(i);
+            moviesItems.add(movie);
+        }
+
+        return moviesItems;
+    }
+
+    private static Movie extractMovie(int index) throws JSONException {
+        JSONObject jsonMovie = sMovieData.getJSONObject(index);
+        return new Movie.Builder()
+                .originalTitle(jsonMovie.getString(ORIGINAL_TITLE_PARAMETER))
+                .overview(OVERVIEW_PARAMETER)
+                .thumbnailPosterPath(POSTER_PATH_PARAMETER)
+                .releaseDate(RELEASE_DATE_PARAMETER)
+                .voteAverage(USER_RATING_PARAMETER).build();
+    }
 }
