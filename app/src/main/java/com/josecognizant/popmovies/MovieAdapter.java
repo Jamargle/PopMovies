2cde2b9411c635392f3b3ca2dee97119dcbeb427
Jose Miguel López Bayona <jmlb0003@gmail.com>
2016-05-25 16:56:51 +0200

    Add provisional adapter and provisional list_item_layout

---
+++ app/src/main/java/com/josecognizant/popmovies/MovieAdapter.java
@@ -0,0 +1,67 @@
+package com.josecognizant.popmovies;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Custom Adapter for RecyclerView in MainActivityFragment
+ * Created by 552702 on 24/05/2016.
+ */
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private OnItemClickListener mClickListener;
+    private List<Movie> mList;
+
+    public MovieAdapter(OnItemClickListener listener, List<Movie> movies) {
+        mList = movies;
+        mClickListener = listener;
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list_movie, parent, false);
+
+        return new MovieViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        String title = mList.get(position).getOriginalTitle();
+        String overview = mList.get(position).getOverview();
+        String releaseDate = mList.get(position).getReleaseDate();
+        String votes = mList.get(position).getVoteAverage();
+        String posterPath = mList.get(position).getThumbnailPosterPath();
+        //TODO:        holder.mMoviePoster
+
+        holder.mMoviePoster.setText(title + "/" + overview + "/" + releaseDate + "/" + votes + "/" + posterPath);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        protected TextView mMoviePoster;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            mMoviePoster = (TextView) itemView.findViewById(R.id.image_container);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            mClickListener.onItemClick(v, getAdapterPosition());
+        }
+    }
+}
