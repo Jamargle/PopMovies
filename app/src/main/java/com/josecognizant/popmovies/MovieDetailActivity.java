e008018d58fafbb20dd23a327effe2c04653c6b2
Jose Miguel López Bayona <jmlb0003@gmail.com>
2016-05-26 12:47:36 +0200

    Add Up button in MovielDetailActivity

--- app/src/main/java/com/josecognizant/popmovies/MovieDetailActivity.java
+++ app/src/main/java/com/josecognizant/popmovies/MovieDetailActivity.java
@@ -1,6 +1,7 @@
 package com.josecognizant.popmovies;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -14,7 +15,17 @@ public class MovieDetailActivity  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
