9fb99359d3c6a59e7fa84ce6a39291fe98fff27f
José López <jmlb0003@red.ujaen.es>
2016-06-05 21:57:20 +0200

    Add callback in MainActivity to respond to the fragment interactions

--- app/src/main/java/com/josecognizant/popmovies/MainActivity.java
+++ app/src/main/java/com/josecognizant/popmovies/MainActivity.java
@@ -1,15 +1,13 @@
 package com.josecognizant.popmovies;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements MainActivityFragment.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public void onItemSelected(Uri dateUri) {
+        showMovieDetails(dateUri);
+    }
+
+    private void showMovieDetails(Uri uri) {
+        Intent intent = new Intent(this, MovieDetailActivity.class).setData(uri);
+        startActivity(intent);
     }
 }
