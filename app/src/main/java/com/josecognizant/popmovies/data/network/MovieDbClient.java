0e079f6b9d551827e74c4226132af38ba942745d
José López <jmlb0003@red.ujaen.es>
2016-06-12 22:41:29 +0200

    Update MovieDbClient to use retrofit to get data

--- app/src/main/java/com/josecognizant/popmovies/data/network/MovieDbClient.java
+++ app/src/main/java/com/josecognizant/popmovies/data/network/MovieDbClient.java
@@ -1,5 +1,6 @@
 package com.josecognizant.popmovies.data.network;
 
+import com.josecognizant.popmovies.domain.model.MoviePage;
 import com.josecognizant.popmovies.model.MovieVideos;
 
 import retrofit2.Call;
@@ -12,6 +13,12 @@ import retrofit2.http.Query;
  * Created by Jose on 06/06/2016.
  */
 public interface MovieDbClient {
+
+    @GET("movie/popular")
+    Call<MoviePage> getListOfPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/top_rated")
+    Call<MoviePage> getListOfTopRatedMovies(@Query("api_key") String apiKey);
 
     @GET("movie/{id}/videos")
     Call<MovieVideos> getListOfVideos(@Path("id") int movieId, @Query("api_key") String apiKey);
