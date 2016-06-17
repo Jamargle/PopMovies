e44ad88abb3c1a312bf8717b4578954abc936c6a
José López <jmlb0003@red.ujaen.es>
2016-06-17 21:27:03 +0200

    Add methods to MoviesView and the main logic to MoviesPresenter

--- app/src/main/java/com/josecognizant/popmovies/presentation/movies/MoviesPresenter.java
+++ app/src/main/java/com/josecognizant/popmovies/presentation/movies/MoviesPresenter.java
@@ -1,23 +1,59 @@
 package com.josecognizant.popmovies.presentation.movies;
 
+import com.josecognizant.popmovies.domain.interactor.LoadMoviesInteractor;
+import com.josecognizant.popmovies.domain.model.Movie;
+import com.josecognizant.popmovies.presentation.InteractorExecutor;
+
+import java.util.List;
+
 /**
  * Presenter for the Movies screen
  * Created by Jose on 14/06/2016.
  */
-public class MoviesPresenter {
+public class MoviesPresenter implements LoadMoviesInteractor.LoadMoviesInteractorOutput {
 
-    private MoviesView mMoviesView;
+    private final LoadMoviesInteractor loadMoviesInteractor;
+    private final InteractorExecutor interactorExecutor;
+    private MoviesView moviesView;
+    private List<Movie> movieList;
 
-    public MoviesPresenter() {
-    }
+    public MoviesPresenter(MoviesView view,
+                           LoadMoviesInteractor loadMoviesInteractor,
+                           InteractorExecutor interactorExecutor) {
 
-    public void onAttach(MoviesView view) {
-        mMoviesView = view;
+        moviesView = view;
+        this.loadMoviesInteractor = loadMoviesInteractor;
+        this.interactorExecutor = interactorExecutor;
     }
 
     public void onDetach() {
-        mMoviesView = null;
+        moviesView = null;
     }
 
+    public void onRefreshMovies() {
+        refreshCurrentLocationForecast();
+    }
 
+    private void refreshCurrentLocationForecast() {
+        moviesView.showLoading();
+        loadMoviesInteractor.setOutput(this);
+        interactorExecutor.execute(loadMoviesInteractor);
+    }
+
+    @Override
+    public void onMoviesLoaded(List<Movie> movieList) {
+        this.movieList = movieList;
+        updateView(movieList);
+    }
+
+    @Override
+    public void onLoadMoviesError() {
+        moviesView.hideLoading();
+        moviesView.showLoadMoviesError();
+    }
+
+    private void updateView(List<Movie> movies) {
+        moviesView.hideLoading();
+        moviesView.updateMoviesToShow(movies);
+    }
 }
--- app/src/main/java/com/josecognizant/popmovies/presentation/movies/MoviesView.java
+++ app/src/main/java/com/josecognizant/popmovies/presentation/movies/MoviesView.java
@@ -1,8 +1,19 @@
 package com.josecognizant.popmovies.presentation.movies;
 
+import com.josecognizant.popmovies.domain.model.Movie;
+
+import java.util.List;
+
 /**
  * View for the Movies screen
  * Created by Jose on 14/06/2016.
  */
 public interface MoviesView {
+    void showLoading();
+
+    void hideLoading();
+
+    void showLoadMoviesError();
+
+    void updateMoviesToShow(List<Movie> movies);
 }
