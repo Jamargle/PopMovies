f3244f4f960f470c25f54f2db066a5be09f73cce
Jose Miguel López Bayona <jmlb0003@gmail.com>
2016-07-12 17:20:17 +0200

    Fix Interactor executor and its implementation

--- app/src/main/java/com/josecognizant/popmovies/presentation/InteractorExecutor.java
+++ app/src/main/java/com/josecognizant/popmovies/presentation/InteractorExecutor.java
@@ -2,21 +2,7 @@ package com.josecognizant.popmovies.presentation;
 
 import com.josecognizant.popmovies.domain.interactor.Interactor;
 
-import java.util.concurrent.ExecutorService;
+public interface InteractorExecutor {
 
-/**
- * Class responsible of execute all interactors in a concurrent way
- * Created by Jose on 24/05/2016.
- */
-public class InteractorExecutor {
-
-    private ExecutorService executorService;
-
-    public InteractorExecutor(ExecutorService executorService) {
-        this.executorService = executorService;
-    }
-
-    public void execute(Interactor interactor) {
-        executorService.execute(interactor);
-    }
+    void execute(Interactor interactor);
 }
