c5f16ef4cf823fde8795fa395ef646ac9de49461
Jose Miguel López Bayona <jmlb0003@gmail.com>
2016-07-12 15:23:21 +0200

    Implementation of interactor executor

--- app/src/main/java/com/josecognizant/popmovies/presentation/InteractorExecutor.java
+++ app/src/main/java/com/josecognizant/popmovies/presentation/InteractorExecutor.java
@@ -1,8 +1,22 @@
 package com.josecognizant.popmovies.presentation;
 
-
 import com.josecognizant.popmovies.domain.interactor.Interactor;
 
-public interface InteractorExecutor {
-    void execute(Interactor interactor);
+import java.util.concurrent.ExecutorService;
+
+/**
+ * Class responsible of execute all interactors in a concurrent way
+ * Created by Jose on 24/05/2016.
+ */
+public class InteractorExecutor {
+
+    private ExecutorService executorService;
+
+    public InteractorExecutor(ExecutorService executorService) {
+        this.executorService = executorService;
+    }
+
+    public void execute(Interactor interactor) {
+        executorService.execute(interactor);
+    }
 }
