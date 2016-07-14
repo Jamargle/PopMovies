87e4d93f9971685c217c475a514f35c16eb66e8b
Jose Miguel López Bayona <jmlb0003@gmail.com>
2016-07-14 11:58:02 +0200

    Provisional interactor to update movies

---
+++ app/src/main/java/com/josecognizant/popmovies/domain/interactor/UpdateMovieInteractor.java
@@ -0,0 +1,17 @@
+package com.josecognizant.popmovies.domain.interactor;
+
+import com.josecognizant.popmovies.domain.model.Movie;
+
+/**
+ * Created by 552702 on 14/07/2016.
+ */
+public class UpdateMovieInteractor implements Interactor {
+    @Override
+    public void run() {
+
+    }
+
+    public void setMovieToUpdate(Movie movie) {
+
+    }
+}
