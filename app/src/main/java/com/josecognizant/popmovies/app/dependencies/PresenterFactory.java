4cbfeceef4442e5b99d45503de62fccf3a7e6962
José López <jmlb0003@red.ujaen.es>
2016-07-25 00:03:34 +0200

    Remove function to create MoviesPresenter from PresenterFactory and use injection in MovieListFragment to inject the presenter

--- app/src/main/java/com/josecognizant/popmovies/app/dependencies/PresenterFactory.java
+++ app/src/main/java/com/josecognizant/popmovies/app/dependencies/PresenterFactory.java
@@ -8,9 +8,6 @@ import com.josecognizant.popmovies.presentation.InteractorExecutorImp;
 import com.josecognizant.popmovies.presentation.details.DetailPresenter;
 import com.josecognizant.popmovies.presentation.details.DetailPresenterImp;
 import com.josecognizant.popmovies.presentation.details.DetailView;
-import com.josecognizant.popmovies.presentation.movies.MoviesPresenter;
-import com.josecognizant.popmovies.presentation.movies.MoviesPresenterImp;
-import com.josecognizant.popmovies.presentation.movies.MoviesView;
 
 import java.util.concurrent.Executors;
 
@@ -18,13 +15,7 @@ import java.util.concurrent.Executors;
  * Helper class to perform presenter creations
  * Created by Jose on 17/06/2016.
  */
-public class PresenterFactory {
-    public static MoviesPresenter makeMoviesPresenter(MoviesView view, Context context) {
-        return new MoviesPresenterImp(view,
-                InteractorFactory.makeLoadMoviesInteractor(context),
-                makeInteractorExecutor());
-    }
-
+public abstract class PresenterFactory {
     public static DetailPresenter makeDetailPresenter(DetailView view, Movie movie, Context context) {
         return new DetailPresenterImp(view, movie,
                 InteractorFactory.makeUpdateMoviesInteractor(context),
