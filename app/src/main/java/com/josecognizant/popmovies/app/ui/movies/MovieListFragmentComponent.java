b3083889d5c4b521a4697d35591a74630fb108f5
José López <jmlb0003@red.ujaen.es>
2016-07-24 22:35:43 +0200

    Create a dagger module for Movie list fragment

---
+++ app/src/main/java/com/josecognizant/popmovies/app/ui/movies/MovieListFragmentComponent.java
@@ -0,0 +1,12 @@
+package com.josecognizant.popmovies.app.ui.movies;
+
+import com.josecognizant.popmovies.app.dependencies.ApplicationComponent;
+import com.josecognizant.popmovies.app.dependencies.scopes.FragmentScope;
+
+import dagger.Component;
+
+@FragmentScope
+@Component(dependencies = ApplicationComponent.class)
+public interface MovieListFragmentComponent {
+    void inject(MovieListFragment fragment);
+}
